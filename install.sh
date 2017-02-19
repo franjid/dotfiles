9f13bc841e8d0a068190f16240ddfbe33d28f929
Fran Iruela <franjid@gmail.com>
2017-02-19 16:50:26 +0100

    Added basic install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Copy git config files to $HOME directory
+cp git/* ~/
+
+# Replace default theme with the one I normally use
+sed -i '' 's/^\(ZSH_THEME\)\(.*\)/ZSH_THEME="avit"/g' ~/.zshrc
+
+# Add custom aliases
+cat zsh/zsh_alias >> ~/.zshrc
